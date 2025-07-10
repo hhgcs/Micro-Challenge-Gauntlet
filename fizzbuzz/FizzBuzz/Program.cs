c38bb3649679e61c1e8f35d3cf5b825c11cc11ca
hhgcs <hehg.cs@gmail.com>
2025-07-09 22:32:13 -0600

    fizzbuzz started, added main function

---
+++ fizzbuzz/FizzBuzz/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Text;
+
+static string FizzBuzz(int n) {
+    StringBuilder result = new StringBuilder("",8);
+    if (n % 3 == 0)
+    {
+        result.Append("Fizz");
+    }
+    if (n % 5 == 0)
+    {
+        result.Append("Buzz");
+    }
+
+    return result.ToString();
+}
+
+
